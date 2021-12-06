d41737128aec432a778e566d64343830c988deab
Josh <jzieve@Joshs-iMac.local>
2021-12-06 15:27:16 -0500

    add feature submit form

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,22 +19,43 @@
 //     alert("button clicked");
 // });
 
-var buttonEl = document.querySelector("#save-task");
+// var buttonEl = document.querySelector("#save-task");  delete
+
+var formEl = document.querySelector("#task-form"); // This will active eventListner all form not just button
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 // buttonEl.addEventListener("click", function() {
 //     var listItemEl = document.createElement("li");
 //     listItemEl.className = "task-item";
-//     listItemEl.textContent = "This is a new task.";
+//     listItemEl.textContent = "This is a new task";
 //     tasksToDoEl.appendChild(listItemEl);
 // });
 
 // code refactor : 
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+    // console.log(event);
+
+    event.preventDefault();
+
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    // Create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task."
+
+    // Create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+
+    //give it a class name
+    taskInfoEl.className = "task-info";
+
+    //add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+
+    // add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
 };
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
